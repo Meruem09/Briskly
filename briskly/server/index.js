2da904520a7e3bb5aad6aff369a1223637fceed2
Rahul Varma <rahulvarma1797@gmail.com>
2025-06-30 16:36:41 +0530

    fixed signup flow made the ui back to intellectup

--- briskly/server/index.js
+++ briskly/server/index.js
@@ -1,4 +1,24 @@
-const express = require('express');
-const cors = require('cors')
-const { PrismaClient } = require('@prisma/client');
-const {clerkMiddleware} = require('@clerk/clerk-react')
+import express from 'express';
+import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+import { requireAuth } from '@clerk/express';
+
+const app = express();
+const PORT = 3000;
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
+
+import userRoutes from './routes/user.js';
+app.use('/user', requireAuth(), userRoutes);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
